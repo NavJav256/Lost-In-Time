b1fc021421f8bfcb0c24dc380efc3b81b8e646d7
NavJav256 <ec19075@qmul.ac.uk>
2023-09-04 18:54:03 +0100

    Added Energy Cube with charging and discharging platforms

---
+++ Source/Lost_In_Time/PuzzleObjects/EnergyCube.h
@@ -0,0 +1,61 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "EnergyCube.generated.h"
+
+UENUM(BlueprintType)
+enum class ECubeState : uint8
+{
+	ECS_IDLE UMETA(DisplayName = "Idle"),
+	ECS_CHARGE UMETA(DisplayName = "Charging"),
+	ECS_DISCHARGE UMETA(DisplayName = "Discharging"),
+
+	ECS_MAX UMETA(DisplayName = "Default Max")
+};
+
+UCLASS()
+class LOST_IN_TIME_API AEnergyCube : public AActor
+{
+	GENERATED_BODY()
+	
+public:
+
+	AEnergyCube();
+
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+
+	virtual void BeginPlay() override;
+
+	void Charge(float DeltaTime);
+
+	void Discharge(float DeltaTime);
+
+private:
+
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* EnergyCube;
+
+	UPROPERTY(VisibleAnywhere)
+	ECubeState CubeState;
+
+	UPROPERTY(EditAnywhere)
+	UMaterialInstance* VoltageMaterialInstance;
+
+	UPROPERTY(VisibleAnywhere)
+	UMaterialInstanceDynamic* DynamicVoltageMaterialInstance;
+
+	UPROPERTY(VisibleAnywhere)
+	float Voltage;
+
+public:
+
+	FORCEINLINE void ChargeCube() { CubeState = ECubeState::ECS_CHARGE; }
+	FORCEINLINE void DischargeCube() { CubeState = ECubeState::ECS_DISCHARGE; }
+	FORCEINLINE void StopCube() { CubeState = ECubeState::ECS_IDLE; }
+
+};
